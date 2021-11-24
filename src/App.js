19442a3cb80dea7be107b502c006c9a32c7ad035
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-24 23:21:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Car from "./Car";
+import Employess from "./Employess";
+import Reducer from "./Reducer";
+// import App1 from "./reducer/App1";
+import Layout from "./Layout";
+import Refhook from "./Refhook";
 
+// const userConnect = createContext()
 
 const Button = styled.button`
 color: blue;
@@ -14,7 +20,11 @@ border-color:red;
 
 `
 
+
 function App() {
+  // const [user, setuser] = useState("ajay")
+  
+
   return (
     <div className="App">
      <label for="username">Username:</label>
@@ -23,8 +33,17 @@ function App() {
      <Input type="password" name="password" />
      <Button>submit</Button>
      <Car />
+     <Employess />
+     {/* <userConnect.Provider value={user}>
+      <App1 />
+     </userConnect.Provider> */}
+
+     <Reducer />
+     <Layout />
+     <Refhook />
     </div>
   );
 }
 
 export default App;
+// export { userConnect }
