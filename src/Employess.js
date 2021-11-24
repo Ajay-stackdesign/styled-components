e3e356815e2e6ec18b8dd16bc204a7969f401721
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-24 23:13:21 +0530

    changes

---
+++ src/Employess.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components"
+
+const Button = styled.button`
+border-radius: 5px;
+color: red;
+background-color: blue;
+`
+
+const Employess = () => {
+    return(
+        <div>
+            <Button>button</Button>
+        </div>
+    )
+}
+
+export default Employess
